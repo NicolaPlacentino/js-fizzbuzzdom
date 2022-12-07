08ade52eaa661af9b4a361389b35a8e676666b18
NicolaPlacentino <nyko0921@gmail.com>
2022-12-07 15:22:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+ per i multipli di 3 stampi “Fizz” al posto del numero
+ per i multipli di 5 stampi “Buzz” al posto del numero.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.*/
+
+//  1. prendo gli elementi dal DOM
+
+const targetElement = document.getElementById('target')
+
+// 2. creo un ciclo che stampi in console i numeri da 1 a 100
+
+for (let i = 1; i <= 100; i++) {
+    
+    let fizzBuzz 
+    
+    // 3. se il numero è multiplo di 3 stampare 'Fizz', se di 5 stampare 'Buzz', se di entrambi stampare 'FizzBuzz'
+    if(i % 3 === 0 && i % 5 === 0){
+        fizzBuzz = 'FizzBuzz'
+    } else if(i % 5 === 0){
+         fizzBuzz = 'Buzz'
+    } else if (i % 3 === 0){
+        fizzBuzz = 'Fizz'
+    }
+
+    if(i % 3 === 0 || i % 5 === 0){
+        console.log(fizzBuzz)
+    } else {
+        console.log(i)
+    }
+}
